33b2e0fe4ac5e2d6c1dac400a7a4070f6448defe
Victor Vbls <victorblsilveira@gmail.com>
2019-12-03 11:06:52 -0300

    Thread model limitations -- part 2

--- src/main/scala/part1recap/ThreadModelLimitations.scala
+++ src/main/scala/part1recap/ThreadModelLimitations.scala
@@ -1,5 +1,7 @@
 package part1recap
 
+import scala.concurrent.Future
+
 object ThreadModelLimitations extends App {
   /*
     Daniel's rants
@@ -10,8 +12,8 @@ object ThreadModelLimitations extends App {
    */
   class BankAccount(private var amount: Int) {
     override def toString: String = "" + amount
-    def withdraw(money: Int) = this.amount -= money
-    def deposit(money: Int) =  this.amount += money
+    def withdraw(money: Int) = this.amount -= money // this.synchronizes { this.amount -= money }
+    def deposit(money: Int) =  this.amount += money // this.synchronizes { this.amount += money }
     def getAmount = amount
   }
 
@@ -36,6 +38,58 @@ object ThreadModelLimitations extends App {
    */
 
   // executor service
+  // you have a running thread and you want to pass a runnable to that thread.
+
+  var task: Runnable = null
+
+  val runningThread:  Thread = new Thread(() => {
+    while (true) {
+      while (task == null) {
+        runningThread.synchronized {
+          println("[background] waiting for a task...")
+          runningThread.wait()
+        }
+
+        task.synchronized {
+          println("[background] I have a task!")
+          task.run()
+          task = null
+        }
+      }
+    }
+  })
+
+  def delegateToBackgroundThread(r: Runnable): Unit = {
+    if(task == null) task = r
+
+    runningThread.synchronized {
+      runningThread.notify()
+    }
+  }
 
 
+  runningThread.start()
+  Thread.sleep(1000)
+  delegateToBackgroundThread(() => println(42))
+  Thread.sleep(1000)
+  delegateToBackgroundThread(() => println("This should run in the background"))
+
+
+  /**
+   * DR #3: Tracing and dealing with errors in a multithreaded env is a PITN.
+   * */
+
+  // 1M number between 10 threads
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  val futures = (1 to 10)
+    .map(i => 100000 * i until 100000 * (i + 1))
+    .map(range => Future {
+      if (range.contains(546735)) throw new RuntimeException("Invalid Number")
+      range.sum
+    })
+
+  val sumFuture = Future.reduceLeft(futures)(_ + _) // Future with the sum of all the numbers
+
+  sumFuture.onComplete(println)
 }
